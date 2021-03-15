9786feb91bd6610e15763f1bd25509dd032c5a7f
ShenSouza <ds052798ts@gmail.com>
2021-03-15 09:53:00 -0300

    Feat: Implementado Video e formulario  #13

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,58 @@
-let nome = window.document.getElementById("nome")
-let email = document.querySelector("#email")
-let assunto = document.querySelector("#assunto")
-let nomeOk = false
-let emailOk = false
-let assuntoOk = false
+let nomeModal = window.document.getElementById("nomeModal")
+let emailModal = document.querySelector("#emailModal")
+let assuntoModal = document.querySelector("#assuntoModal")
+let nomeModalOk = false
+let emailModalOk = false
+let assuntoModalOk = false
 let mapa = document.querySelector("#mapa")
 
-nome.style.width = "100%"
-email.style.width = "100%"
+nomeModal.style.width = "100%"
+emailModal.style.width = "100%"
 
-function validaNome() {
-    let txtNome = document.querySelector("#txtNome")
-    if(nome.value.length < 3) {
-        txtNome.innerHTML = "Nome inválido"
-        txtNome.style.color = "red"
-        let nomeOk = false
+function validanomeModal() {
+    let txtnomeModal = document.querySelector("#txtnomeModal")
+    if(nomeModal.value.length < 3) {
+        txtnomeModal.innerHTML = "nomeModal inválido"
+        txtnomeModal.style.color = "red"
+        let nomeModalOk = false
     } else {
-        txtNome.innerHTML = "Nome Válido"
-        txtNome.style.color = "green"
-        nomeOk = true
+        txtnomeModal.innerHTML = "nomeModal Válido"
+        txtnomeModal.style.color = "green"
+        nomeModalOk = true
     }
 }
 
-function validaEmail() {
-    let txtEmail = document.querySelector("#txtEmail")
-    if(email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
-        txtEmail.innerHTML = "E-mail Inválido!"
-        txtEmail.style.color = "red"
-        let emailOk = false
+function validaemailModal() {
+    let txtemailModal = document.querySelector("#txtemailModal")
+    if(emailModal.value.indexOf("@") == -1 || emailModal.value.indexOf(".") == -1) {
+        txtemailModal.innerHTML = "E-mail Inválido!"
+        txtemailModal.style.color = "red"
+        let emailModalOk = false
     } else {
-        txtEmail.innerHTML = "E-mail Válido"
-        txtEmail.style.color = "green"
-        emailOk = true
+        txtemailModal.innerHTML = "E-mail Válido"
+        txtemailModal.style.color = "green"
+        emailModalOk = true
     }
 }
 
-function validaAssunto() {
-    let txtAssunto = document.querySelector("#txtAssunto")
-    if(assunto.value.length >= 100) {
-        txtAssunto.innerHTML = "Texto é muito grande, digite ao maximo 100 caracteres"
-        txtAssunto.style.color = "red"
-        txtAssunto.style.display = "block"
-        let assuntoOk = false
+function validaassuntoModal() {
+    let txtassuntoModal = document.querySelector("#txtassuntoModal")
+    if(assuntoModal.value.length >= 100) {
+        txtassuntoModal.innerHTML = "Texto é muito grande, digite ao maximo 100 caracteres"
+        txtassuntoModal.style.color = "red"
+        txtassuntoModal.style.display = "block"
+        let assuntoModalOk = false
     } else {
-        txtAssunto.style.display = "none"
-        assuntoOk = true
+        txtassuntoModal.style.display = "none"
+        assuntoModalOk = true
     }
 }
 
 function enviar() {
-    if(nomeOk == true && emailOk == true && assuntoOk == true) {
+    if(nomeModalOk == true && emailModalOk == true && assuntoModalOk == true) {
         alert("Formulário enviado com sucesso!")
     } else {
-        alert("Preencha o formulário corretamente antes de enviar...")
+        alert("Preencha o formulário corretamente antes de enviar...")  
     }
 }
 
